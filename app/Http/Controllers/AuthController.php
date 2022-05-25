0e6a02251b99ac541d90966440697f8440469dc2
zhaksylyk <nartai53@gmail.com>
2022-05-25 23:33:29 +0600

    created AdminUser model and make migrations of admin_users_tables And make authorization

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+namespace App\Http\Controllers;
+
+use App\Jobs\ForgotUserEmailJob;
+use App\Mail\ForgotPassword;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class AuthController extends Controller
+{
+    public function showLoginForm(){
+        return view("auth.login");
+    }
+
+    public function logout(){
+        auth("web")->logout();
+
+        return redirect(route("home"));
+    }
+
+    public function showRegisterForm(){
+        return view("auth.register");
+    }
+
+    public function login(Request $request){
+        $data = $request->validate([
+            "email" => ["required", "email", 'string', "exists:users,email"],
+            "password"=>["required"],
+
+        ]);
+
+        if(auth("web")->attempt($data)){
+            return redirect(route("home"));
+        }
+
+        return redirect(route('login'));
+    }
+
+    public function register(Request $request){
+        $data = $request->validate([
+            "name" => ["required", "string"],
+            "email" => ["required", "email", 'string', "unique:users,email"],
+            "password"=>["required", "confirmed"],
+
+        ]);
+
+        $user = User::create([
+           'name' => $data["name"],
+            'email' => $data["email"],
+            'password' => bcrypt($data["password"])
+        ]);
+
+        if ($user) {
+            auth("web")->login($user);
+        }
+
+        return redirect(route('home'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,14 @@ Route::group(['namespace' => 'App\Http\Controllers'], function (){
 
     Route::get('/', 'StartController@home')->name('home');
 
-    Route::get('/create-habit', 'UserController@userPanel')->name('create-habit');
+    Route::get('/create-habit', 'HabitController@userPanel')->name('create-habit');
+
+    Route::get('/login', 'AuthController@showLoginForm')->name('login');
+    Route::get('/logout', 'AuthController@logout')->name('logout');
+    Route::post('/login_process', 'AuthController@login')->name('login_process');
+
+    Route::get('/register', 'AuthController@showRegisterForm')->name('register');
+    Route::post('/register_process', 'AuthController@register')->name('register_process');
+
 });
 
