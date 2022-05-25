58863a0a2ad55408988eac4b9bb4e5d5ecf90ac2
zhaksylyk <nartai53@gmail.com>
2022-05-25 22:52:48 +0600

    added categories page and  modified register page

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,16 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-Route::get('/statistics', function () {
-    return view('statistics');
-})->name('statistics');
+Route::get('/categories', function () {
+    return view('categories');
+})->name('categories');
 
 Route::get('/register', function () {
     return view('register');
 })->name('register');
+
+Route::get('/create-habit', function () {
+    return view('create-habit');
+})->name('create-habit');
 
 Route::post('/register/submit', 'RegisterController@submit')->name('register-form');
