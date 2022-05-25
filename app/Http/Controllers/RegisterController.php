d3bdd3845b84bf33c0ce5446598fca28667921ae
ademaaitu <97827835+ademaaitu@users.noreply.github.com>
2022-05-25 16:46:54 +0600

    test version of registerController

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+
+class RegisterController extends Controller
+{
+    public function submit(RegisterRequest $req){
+
+    }
+}
